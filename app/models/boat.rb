09ef47350a6e4c89e7d0d39e614f4bf5511ecfe1
Ayana Bando <ayanaflax@yahoo.com>
2019-02-13 01:24:30 +0000

    Automatically backed up by Learn

---
+++ app/models/boat.rb
@@ -0,0 +1,22 @@
+class Boat < ActiveRecord::Base
+  belongs_to  :captain
+  has_many    :boat_classifications
+  has_many    :classifications, through: :boat_classifications
+    
+  def self.sailboats
+    includes(:classifications).where(classifications: {name: 'Sailboat'})
+  end
+  
+  def self.first_five
+    all.limit(5)
+  end
+  
+    def self.last_three_alphabetically
+    all.order(name: :desc).limit(3)
+  end
+  
+  def self.dinghy
+    where("length < 20")
+  end
+  
+end
