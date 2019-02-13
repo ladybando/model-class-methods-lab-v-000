dc04fe9ea946ea14880bab8ff2d2e1c2ae29c19f
Ayana Bando <ayanaflax@yahoo.com>
2019-02-13 01:22:57 +0000

    Automatically backed up by Learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,5 +2,13 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
-
+    
+  def sailboats
+  end
+  
+  def first_five
+  end
+  
+  def dinghy
+  end
 end
