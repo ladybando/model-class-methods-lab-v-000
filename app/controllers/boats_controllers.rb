8fcb772c091d442e1b8292d0a12e3846d83a31c9
Ayana Bando <ayanaflax@yahoo.com>
2019-02-13 01:20:44 +0000

    Automatically backed up by Learn

---
+++ app/controllers/boats_controllers.rb
@@ -0,0 +1,8 @@
+class BoatController < ApplicationController
+  def index
+
+  end
+    def sailboats
+  end
+  
+end
---
+++ app/helpers/boat_helper.rb
@@ -0,0 +1,11 @@
+module BoatHelper
+    
+  def sailboats
+  end
+  
+  def first_five
+  end
+  
+  def dinghy
+  end
+end
